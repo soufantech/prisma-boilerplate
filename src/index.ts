6ca3db15ff38f585cca126b8d5a8e5510398054a
Igor Silveira <igor-silveira10@hotmail.com>
2021-04-08 19:49:25 -0300

    chore: update imports, add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import 'reflect-metadata'
-import * as tq from 'type-graphql'
-import { ApolloServer } from 'apollo-server'
-import { context } from './context'
-import { resolvers } from '@generated/type-graphql'
+import 'reflect-metadata';
+import * as tq from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { context } from './context';
+import { resolvers } from '@generated/type-graphql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = async () => {
   const schema = await tq.buildSchema({
     resolvers,
   })
 
-  new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
+  new ApolloServer({ schema, context: context }).listen({ port: process.env.PORT }, () =>
     console.log(
-      `Graphql docs: http://localhost:4000`,
+      `Graphql docs: http://localhost:${process.env.PORT}`,
     ),
   )
 }
 
-app()
+app();
